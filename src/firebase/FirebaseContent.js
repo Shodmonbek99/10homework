7564af636c952c8e966205d163697d29d18b9684
Shodmonbek-06 <shodmonbekakbarov02@gmail.com>
2024-05-06 20:40:08 +0500

    new project

---
+++ src/firebase/FirebaseContent.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCfnTjeaoodrnhkcVX54zlclSFZdZSPeZA",
+//   authDomain: "mybook-cca45.firebaseapp.com",
+//   projectId: "mybook-cca45",
+//   storageBucket: "mybook-cca45.appspot.com",
+//   messagingSenderId: "121196979712",
+//   appId: "1:121196979712:web:d0c914b1ec725c7d4e5ae5",
+//   measurementId: "G-W7EZEWMDEW"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
